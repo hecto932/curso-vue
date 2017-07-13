ee7e6a4cb21587f2e0a4a4b74ca31177a3a468e4
Hector Flores <hecto932@gmail.com>
2017-07-13 13:20:40 -0500

    Ejercicio de SASS y PUG

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-  </div>
+<template lang="pug">
+  #app
+    img(src='./assets/platzi-station.jpg')
+    h1 {{ msg }}
+    form
+      .container-elment
+        label(for="name" class="container-label") Nombre
+        input(name="name" class="container-input")
+      .container-elment
+        label(for="lastname" class="container-label") Apellido
+        input(name="lastname" class="container-input")
+      .container-elment
+        label(for="role" class="container-label") Cargo
+        input(name="role" class="container-input")
+      .container-submit
+        button(class="container-btn") Enviar
+
 </template>
 
 <script>
