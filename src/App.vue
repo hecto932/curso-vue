d893feed0699b28f302ada2c17ca51883c9221ab
Hector Flores <hecto932@gmail.com>
2017-07-19 14:39:53 -0500

    Events

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template lang="pug">
   #app
     input(v-model="name")
-    input(v-model="lastname")
-    input(type="date" v-model="birthdate" value="birthdate")
+    button(@click="format") Formart 
 
-    p {{ fullName }}
-    p {{ getAge }}
+    p {{ formatedName }}
 </template>
 
 <script>
@@ -13,25 +11,13 @@ export default {
   name: 'app',
   data () {
     return {
-      name: 'Hector',
-      lastname: 'Flores',
-      birthdate: new Date().toISOString().slice(0, 10)
+      name: '',
+      formatedName: ''
     }
   },
-  computed: {
-    fullName () {
-      return `${this.name} ${this.lastname}`
-    },
-    getAge () {
-      let birthdate = new Date(this.birthdate)
-      let ageDifMs = Date.now() - birthdate.getTime()
-      let ageDate = new Date(ageDifMs)
-      return Math.abs(ageDate.getUTCFullYear() - 1970)
-    }
-  },
-  watch: {
-    name (newValue, oldValue) {
-      console.log()
+  methods: {
+    format () {
+      this.formatedName = this.name.split(' ').join('-')
     }
   }
 }
