73c069ca682aad23e95867caeb7d71cca834ed61
Hector Flores <hecto932@gmail.com>
2017-08-09 18:53:49 -0500

    Creacion de componentes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   #app
+    pm-header
     section.section
       nav.nav.has-shadow
         .container
@@ -19,12 +20,16 @@
         .columns
           .column(v-for="t in tracks") 
             | {{ t.name }} - {{ t.artists[0].name }}
+    pm-footer
 </template>
 <script>
 import trackService from './services/track'
+import PmHeader from './components/layout/Header.vue'
+import PmFooter from './components/layout/Footer.vue'
 
 export default {
   name: 'app',
+  components: { PmFooter, PmHeader },
   data () {
     return {
       searchQuery: '',
---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  footer.footer
+    .container
+      .content.has-text-centered
+        p Code with love by 
+          a(href="https://github.com/hect932")  hecto932
+</template>
