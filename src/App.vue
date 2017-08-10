ec744f123276ed934592d81dae365df28fe9a177
Hector Flores <hecto932@gmail.com>
2017-08-10 18:56:18 -0500

    Comunicacion dde hijos hacia padres

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
     pm-loader(v-show="isLoading")
     section.section(v-show="!isLoading")
-      nav.nav.has-shadow
+      nav.nav
         .container
           input.input.is-large(
             type="text",
@@ -20,7 +20,7 @@
       .container.results
         .columns.is-multiline
           .column.is-one-quarter(v-for="t in tracks")
-            pm-track(:track="t")
+            pm-track(:track="t", @select="setSelectedTrack", :class="{ 'is-active': t.id === selectedTrack }")
 
     pm-footer
 </template>
@@ -43,7 +43,8 @@ export default {
       searchQuery: '',
       tracks: [],
 
-      isLoading: false
+      isLoading: false,
+      selectedTrack: ''
     }
   },
 
@@ -64,6 +65,9 @@ export default {
           this.tracks = res.tracks.items
           this.isLoading = false
         })
+    },
+    setSelectedTrack (id) {
+      this.selectedTrack = id
     }
   }
 }
@@ -74,5 +78,9 @@ export default {
 
   .results {
     margin-top: 50px;
+  }
+
+  .is-active {
+    border: 3px #23d160 solid;
   }
 </style>
