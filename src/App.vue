ad4821452d4b631edca58366b080593b54e22bb5
Hector Flores <hecto932@gmail.com>
2017-08-21 16:38:12 -0500

    Instalar vue-router y configurar router view

--- src/App.vue
+++ src/App.vue
@@ -2,96 +2,18 @@
   #app
     pm-header
 
-    pm-notification(:typeNotification="hasData", v-show="showNotification")
-      p(v-if="hasData" slot="body") No se encontraron resultados
-      p(v-else slot="body") {{ searchMessage }}
-
-    pm-loader(v-show="isLoading")
-    section.section(v-show="!isLoading")
-      nav.nav
-        .container
-          input.input.is-large(
-            type="text",
-            placeholder="Buscar canciones",
-            v-model="searchQuery"
-          )
-          a.button.is-info.is-large(@click="search") Buscar
-          a.button.is-danger.is-large &times;
-      .container
-        p
-          small {{ searchMessage }}
-
-      .container.results
-        .columns.is-multiline
-          .column.is-one-quarter(v-for="t in tracks")
-            pm-track(:track="t", @select="setSelectedTrack", :class="{ 'is-active': t.id === selectedTrack }")
+    router-view
 
     pm-footer
 </template>
 
 <script>
-import trackService from '@/services/track'
-
 import PmFooter from '@/components/layout/Footer.vue'
 import PmHeader from '@/components/layout/Header.vue'
 
-import PmTrack from '@/components/Track.vue'
-
-import PmNotification from '@/components/shared/Notification.vue'
-import PmLoader from '@/components/shared/Loader.vue'
-
 export default {
   name: 'app',
-
-  components: { PmFooter, PmHeader, PmTrack, PmLoader, PmNotification },
-
-  data () {
-    return {
-      searchQuery: '',
-      tracks: [],
-
-      isLoading: false,
-      showNotification: false,
-      selectedTrack: '',
-      hasData: false
-    }
-  },
-
-  computed: {
-    searchMessage () {
-      return `Encontrados: ${this.tracks.length}`
-    }
-  },
-
-  watch: {
-    showNotification () {
-      if (this.showNotification) {
-        setTimeout(() => {
-          this.showNotification = false
-        }, 3000)
-      }
-    }
-  },
-
-  methods: {
-    search () {
-      if (!this.searchQuery) { return }
-
-      this.isLoading = true
-
-      trackService.search(this.searchQuery)
-        .then(res => {
-          console.log(res)
-          this.showNotification = true
-          this.hasData = res.tracks.total === 0
-          this.tracks = res.tracks.items
-          this.isLoading = false
-        })
-    },
-    setSelectedTrack (id) {
-      this.selectedTrack = id
-    }
-  }
+  components: { PmFooter, PmHeader }
 }
 </script>
 
---
+++ src/components/Search.vue
@@ -0,0 +1,101 @@
+<template lang="pug">
+  main
+
+    pm-notification(:typeNotification="hasData", v-show="showNotification")
+      p(v-if="hasData" slot="body") No se encontraron resultados
+      p(v-else slot="body") {{ searchMessage }}
+
+    pm-loader(v-show="isLoading")
+    section.section(v-show="!isLoading")
+      nav.nav
+        .container
+          input.input.is-large(
+            type="text",
+            placeholder="Buscar canciones",
+            v-model="searchQuery"
+          )
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+      .container
+        p
+          small {{ searchMessage }}
+
+      .container.results
+        .columns.is-multiline
+          .column.is-one-quarter(v-for="t in tracks")
+            pm-track(:track="t", @select="setSelectedTrack", :class="{ 'is-active': t.id === selectedTrack }")
+
+</template>
+
+<script>
+import trackService from '@/services/track'
+
+import PmTrack from '@/components/Track.vue'
+
+import PmNotification from '@/components/shared/Notification.vue'
+import PmLoader from '@/components/shared/Loader.vue'
+
+export default {
+  name: 'app',
+
+  components: { PmTrack, PmLoader, PmNotification },
+
+  data () {
+    return {
+      searchQuery: '',
+      tracks: [],
+
+      isLoading: false,
+      showNotification: false,
+      selectedTrack: '',
+      hasData: false
+    }
+  },
+
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
+    }
+  },
+
+  watch: {
+    showNotification () {
+      if (this.showNotification) {
+        setTimeout(() => {
+          this.showNotification = false
+        }, 3000)
+      }
+    }
+  },
+
+  methods: {
+    search () {
+      if (!this.searchQuery) { return }
+
+      this.isLoading = true
+
+      trackService.search(this.searchQuery)
+        .then(res => {
+          console.log(res)
+          this.showNotification = true
+          this.hasData = res.tracks.total === 0
+          this.tracks = res.tracks.items
+          this.isLoading = false
+        })
+    },
+    setSelectedTrack (id) {
+      this.selectedTrack = id
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .results {
+    margin-top: 50px;
+  }
+
+  .is-active {
+    border: 3px #23d160 solid;
+  }
+</style>
